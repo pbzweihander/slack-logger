b6c397e42a24c94f0ed069e1d179cec11fb69fe4
pbzweihander <sd852456@naver.com>
2017-06-07 14:34:20 +0900

    handle_message 함수와 log_to_json 함수로 분리

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from slack import Slack
 from datetime import datetime
+from elasticsearch import Elasticsearch
 import settings
 import time
 import json
@@ -7,7 +8,6 @@ import logging
 import logging.handlers
 
 slack = Slack(settings.SLACK_TOKEN, settings.BOT_NAME)
-slack.connect()
 logger = logging.getLogger('slack-logger')
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(message)s')
@@ -17,10 +17,24 @@ file_handler.suffix = "%Y-%m-%d"
 stream_handler = logging.StreamHandler()
 logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
+es_client = Elasticsearch()
 
 
-def log_to_json(channel: str, user: str, text: str, ts: float):
-    outdict = dict()
+def es_create(index: str, doc_type: str, body: dict) -> bool:
+    res = es_client.create(index, doc_type, body)
+    return res.get('created') or False
+
+
+def es_search(index: str, doc_type: str, body: dict) -> list:
+    query = { 'query': { 'term': {} } }
+    query['query']['term'].update(body)
+    res = es_client.search(index, doc_type, query)
+    if res.get('hits') and res['hits'].get('hits'):
+        return [doc.get('_source') for doc in res['hits']['hits']]
+    return []
+
+
+def handle_message(channel:str, user: str, text: str, ts: float):
     channame = slack.channels.get(channel)
     if not channame:
         slack.refresh_channels()
@@ -29,12 +43,21 @@ def log_to_json(channel: str, user: str, text: str, ts: float):
     if not username:
         slack.refresh_users()
         username = slack.users.get(user) or "?"
+    date_ts = datetime.fromtimestamp(ts)
+    log_to_json(channame, username, text, date_ts)
+    return ""
+
+
+def log_to_json(channel: str, user: str, text: str, date_ts: datetime):
+    outdict = dict()
     outdict['channel'] = channame
     outdict['user'] = username
     outdict['text'] = text
-    outdict['time'] = datetime.fromtimestamp(ts).strftime("%Y-%m-%d %H:%M:%S")
+    outdict['ts'] = dict()
+    outdict['ts']['date'] = date_ts.strftime('%Y-%m-%d')
+    outdict['ts']['time'] = date_ts.strftime('%H:%M:%S')
     logger.info(json.dumps(outdict, ensure_ascii=False))
-    return ""
+    es_create(settings.ES_INDEX, setting.ES_TYPE, outdict)
 
 
 def main():
@@ -43,7 +66,7 @@ def main():
         texts = slack.read()
         if not texts:
             continue
-        _ = [log_to_json(text.get('channel'), text.get('user'), text.get('text'), float(text.get('ts')))
+        _ = [handle_message(text.get('channel'), text.get('user'), text.get('text'), float(text.get('ts')))
              for text in texts
              if text.get('type') == 'message'
              if 'subtype' not in text
@@ -53,3 +76,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
