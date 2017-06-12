b1bf35d343593d05a54cfda8053cdd95052bf740
pbzweihander <sd852456@naver.com>
2017-06-12 13:05:21 +0900

    라이센스 추가

--- slack_logger.py
+++ slack_logger.py
@@ -1,3 +1,19 @@
+# Name: slack_logger.py
+# Author: pbzweihander
+# Email: sd852456@naver.com
+#
+# Copyright (C) 2017 pbzweihander
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
 import datetime
 import elasticsearch_wrapper
 import slack_wrapper
